fd01706fd6b207cba74a21aa620bb0ca2c66a779
kubastra <j.straupisz@gmail.com>
2025-04-29 09:30:13 +0200

    test

--- kopiec.h
+++ kopiec.h
@@ -1,32 +1,79 @@
 #ifndef KOPIEC_H
 #define KOPIEC_H
 
-#include "element.h"
+#include <iostream>
 #include <vector>
-
+#include <stdexcept>
 
 class Kopiec{
     private:
-        std::vector<Element> elements;
+        std::vector<int> elements;
+        int parent(int i) {
+            return (i - 1) / 2;
+        }
+        int left(int i) {
+            return 2 * i + 1;
+        }
+        int right(int i) {
+            return 2 * i + 2;
+        }
+
+        void heapifyUp(int i) {
+            while (i > 0 && elements[i] < elements[parent(i)]) {
+                std::swap(elements[i], elements[parent(i)]);
+                i = parent(i);
+            }
+        }
 
     public:
         Kopiec() {
             elements.clear();
         }
+        void heapifyDown(int i) {
+            int l = left(i);
+            int r = right(i);
+            int smallest = i;
+            int n = elements.size();
+            if (l < n && elements[l] < elements[smallest]) {
+                smallest = l;
+            }
+            if (r < n && elements[r] < elements[smallest]) {
+                smallest = r;
+            }
+            if (smallest != i) {
+                std::swap(elements[i], elements[smallest]);
+                heapifyDown(smallest);
+            }
+        }
 
-        void insert(Element &element) {
+        void inserta(int element) {
             elements.push_back(element);
+            heapifyUp(elements.size() - 1);
         }
 
         void pop_min() {
-            elements.erase(elements.begin());
+            elements[0] = elements.back();
+            elements.pop_back();
+            heapifyDown(0);
         }
 
-        void print() {
-            for (int i = 0; i < elements.size(); i++) {
-                std::cout << elements[i].getPriority() << " ";
+    void print() {
+            int n = elements.size();
+            int level = 0;
+            int index = 0;
+
+            while (index < n) {
+                int nodesInLevel = 1 << level; // 2^level
+                int spaces = (1 << (4 - level)); // dobierz liczbę jak trzeba
+
+                for (int i = 0; i < nodesInLevel && index < n; ++i) {
+                    std::cout << std::string(spaces, ' ') << "[" << elements[index++] << "]";
+                }
+                std::cout << "\n";
+                level++;
             }
         }
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,59 @@
 #include "kopiec.h"
+#include <chrono>
+#include <fstream>
 
+std::ofstream file("time.dat");
+#define MAX 10000
+#define REPEAT 10
 
-
-int main() {
-
-    Element el1 = Element(10);
-    Element el2 = Element(20);
-
+void test() {
     Kopiec kop;
 
-    kop.insert(el1);
-    kop.insert(el2);
+    kop.inserta(10);
+    kop.inserta(12);
+    kop.inserta(13);
+    kop.inserta(14);
+    kop.inserta(15);
+    kop.inserta(16);
+
+    kop.print();
+    std::cout << std::endl;
 
     kop.pop_min();
 
     kop.print();
+}
 
+void pop_measure(int size) {
+    auto dur = 0;
+    for (int l = 0; l < REPEAT; l++){
+
+        Kopiec kop;
+
+        for (int i = size; i > 0; i--) {
+            kop.inserta(i);
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+        kop.pop_min();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        dur+=duration;
+    }
+        dur = dur/REPEAT;
+
+    file << size << " " << dur << "\n";
+}
+int main() {
+    if (!file.is_open()) {
+        std::cout << "file not open";
+        return -1;
+    }
+
+    for (int i = 1; i < MAX; i++) {
+        pop_measure(i);
+        if (i > 50 ) i += 10;
+    }
 
     return 0;
 }
